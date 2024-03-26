77d5d1af2edff65ebf721183d7ce9a10992143a0
Sodikcp <paramesh.rah@gmail.com>
2024-03-26 18:24:14 +0700

    finish

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,33 @@
 //Exercise 1: Roman to Integer
 
-const romanToInt = function (s) {
-  //Start coding here
-};
+function romanToInt(s) {
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    let number = roman[s[i]];
+    console.log(number);
+    let nextNumber = roman[s[i + 1]];
+    console.log(nextNumber);
+    if (number > nextNumber) {
+      sum += number;
+    } else if (number < nextNumber) {
+      sum -= number;
+    } else {
+      sum += number;
+    }
+  }
+  console.log(sum);
+  return sum;
+}
 
 const result1 = romanToInt("III"); // 3
 const result2 = romanToInt("LVIII"); // 58
--- ex-2.js
+++ ex-2.js
@@ -2,12 +2,32 @@
 
 const fizzBuzz = function (n) {
   //Start coding here
+  let newArray = [];
+  console.log(n);
+
+  for (let i = 0; i < n; i++) {
+    if ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
+      newArray.push("FizzBuzz");
+    } else if ((i + 1) % 5 === 0) {
+      newArray.push("Buzz");
+    } else if ((i + 1) % 3 === 0) {
+      newArray.push("Fizz");
+    } else {
+      newArray.push(i + 1);
+    }
+
+    console.log(i);
+  }
+  return newArray;
 };
 
 const result1 = fizzBuzz(3);
 const result2 = fizzBuzz(5);
 const result3 = fizzBuzz(15);
 
-console.log(result1); // ["1","2","Fizz"]
-console.log(result2); // ["1","2","Fizz","4","Buzz"]
-console.log(result3); // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+console.log(result1);
+// ["1","2","Fizz"]
+console.log(result2);
+// ["1","2","Fizz","4","Buzz"]
+console.log(result3);
+// ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
